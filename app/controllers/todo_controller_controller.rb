49b53948dc0d64a3e66f676dafe984adb04af188
Slava <slava.eg@yandex.ru>
2017-09-25 16:35:10 +0300

    All works

---
+++ app/controllers/todo_controller_controller.rb
@@ -0,0 +1,16 @@
+class TodoControllerController < ApplicationController
+  def index
+  	#@projects = Project.all
+  end
+
+  def newTodo
+  	proj = Project.find(params[:project_id])
+	todo = Todo.create text: params[:text]
+  	proj.todos << todo
+	redirect_to "/"
+  end
+
+  def updateTodos
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  root to: 'todo_controller#index'
+
+  get 'todo_controller/index'
+
+  get 'todo_controller/newTodo'
+  post 'todo_controller/newTodo'
+
+  get 'todo_controller/updateTodos'
+  post 'todo_controller/updateTodos'
+
+  get 'todo_controller/createTodo'
+  post 'todo_controller/createTodo'
+
+  #get 'todo_controller/createTodo' to: 'todo_controller#createTodo'
+ # post 'todo_controller/createTodo' to: 'todo_controller#createTodo'
+
+	#get '/todo_controller/:Todo', to: 'todo_controller#createTodo', as: 'todo'
+
+	match 'todos', to: 'todo_controller#createTodo', via: [:post]
+  #resources :projects
+  resources :todos, controller: 'todo_controller'
+  #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
